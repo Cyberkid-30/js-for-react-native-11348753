e728a9b4d66d5e2bfb4a5422b40105444af6a12b
Jan Phandoh <janphandoh@gmail.com>
2024-05-17 11:28:53 +0000

    Task 1 done..

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,11 +1,10 @@
-
 //Task 1
 function processArray(array) {
   const newArray = array.map((item) => {
     if (item % 2 === 0) {
       return item * item;
     } else {
-      return item * item * item;
+      return item * 3;
     }
   });
 
